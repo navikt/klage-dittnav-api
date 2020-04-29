6d0428986a7fcc7110fa32a6bcbe9a0f7e208df8
Andreas Jonsson <andreas.jonsson@nav.no>
2020-04-29 12:55:44 +0200

    Preparing for Kafka

---
+++ src/kotlin/no/nav/klage/config/KafkaConfiguration.kt
@@ -0,0 +1,50 @@
+package no.nav.klage.config
+
+import org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SaslConfigs
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import java.util.*
+
+
+@Configuration
+class KafkaConfiguration {
+
+    @Value("\${KAFKA_BOOTSTRAP_SERVERS}")
+    lateinit var bootstrapServers: String
+
+    @Value("\${KAFKA_CLIENTID}")
+    lateinit var clientId: String
+
+    @Value("\${SERVICE_USER_USERNAME}")
+    lateinit var username: String
+
+    @Value("\${SERVICE_USER_PASSWORD}")
+    lateinit var password: String
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ProducerConfig.CLIENT_ID_CONFIG] = clientId
+        configProps[ProducerConfig.ACKS_CONFIG] = "all"
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[SaslConfigs.SASL_JAAS_CONFIG] =
+            "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$username\" password=\"$password\";"
+        configProps[SaslConfigs.SASL_MECHANISM] = "PLAIN"
+        configProps[SECURITY_PROTOCOL_CONFIG] = "SASL_PLAINTEXT"
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+}
