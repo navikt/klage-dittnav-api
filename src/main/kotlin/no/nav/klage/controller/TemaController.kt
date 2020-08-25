b277bc4a6f3d7b92d43f6d4b3ae624a40671fe59
Andreas Jonsson <andreas.jonsson@nav.no>
2020-08-25 14:33:56 +0200

    get tema

--- src/main/kotlin/no/nav/klage/controller/TemaController.kt
+++ src/main/kotlin/no/nav/klage/controller/TemaController.kt
@@ -2,16 +2,28 @@ package no.nav.klage.controller
 
 import no.nav.klage.domain.Tema
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Unprotected
 class TemaController {
 
-    @GetMapping("/temaer")
-    fun getTemaer(): Array<Tema> {
-        return Tema.values()
+    @GetMapping("/temaer/{code}")
+    fun getTema(@PathVariable code: String): ResponseEntity<TemaResponse> {
+        return try {
+            ResponseEntity.ok(Tema.valueOf(code).toResponse())
+        } catch (iae: IllegalArgumentException) {
+            ResponseEntity.notFound().build()
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
     }
-}
 
+    private fun Tema.toResponse() = TemaResponse(this.name, this.beskrivelse)
+
+    data class TemaResponse(val code: String, val value: String)
+}
