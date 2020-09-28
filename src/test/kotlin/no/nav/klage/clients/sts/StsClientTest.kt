661fdb2eab5bb4de275d8d1880a0e8708161fb1b
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2020-09-28 10:23:38 +0200

    Updated test.

--- src/test/kotlin/no/nav/klage/clients/sts/StsClientTest.kt
+++ src/test/kotlin/no/nav/klage/clients/sts/StsClientTest.kt
@@ -1,17 +1,37 @@
 package no.nav.klage.clients.sts
 
+import io.github.resilience4j.retry.Retry
+import io.github.resilience4j.retry.RetryConfig
+import io.github.resilience4j.retry.RetryRegistry
 import no.nav.klage.clients.StsClient
 import no.nav.klage.clients.createShortCircuitWebClient
 import no.nav.klage.clients.createShortCircuitWebClientQueued
+import no.nav.slackposter.SlackClient
 import org.intellij.lang.annotations.Language
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.Duration
 
 class StsClientTest {
 
+    @Autowired
+    val slackClient = SlackClient("", "", "", "")
+
+    private val retryConfig: RetryConfig = RetryConfig
+            .custom<RetryConfig>()
+            .maxAttempts(5)
+            .waitDuration(Duration.ofSeconds(3))
+            .retryExceptions(RuntimeException::class.java)
+            .build()
+
+    private val retryRegistry = RetryRegistry.of(retryConfig)
+
+    private val retrySts: Retry = retryRegistry.retry("STS")
+
     @Test
     fun `should provide correct access token`() {
-        val stsClient = StsClient(createShortCircuitWebClient(defaultToken))
+        val stsClient = StsClient(createShortCircuitWebClient(defaultToken), slackClient, retrySts)
         val token = stsClient.oidcToken()
 
         assertEquals("default access token", token)
@@ -20,7 +40,7 @@ class StsClientTest {
     @ExperimentalStdlibApi
     @Test
     fun `should refresh token`() {
-        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(shortLivedToken, defaultToken))
+        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(shortLivedToken, defaultToken), slackClient, retrySts)
         val token1 = stsDefaultClient.oidcToken()
         val token2 = stsDefaultClient.oidcToken()
 
@@ -31,7 +51,7 @@ class StsClientTest {
     @ExperimentalStdlibApi
     @Test
     fun `should cache token`() {
-        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(defaultToken, shortLivedToken))
+        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(defaultToken, shortLivedToken), slackClient, retrySts)
         val token1 = stsDefaultClient.oidcToken()
         val token2 = stsDefaultClient.oidcToken()
 
