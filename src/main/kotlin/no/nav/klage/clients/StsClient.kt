e7f1a7173c687c6be4c53c802c11d67df76b4b32
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2020-09-28 13:19:13 +0200

    Moved common functions to util class, altered to avoid null error.

--- src/main/kotlin/no/nav/klage/clients/StsClient.kt
+++ src/main/kotlin/no/nav/klage/clients/StsClient.kt
@@ -1,21 +1,23 @@
 package no.nav.klage.clients
 
 import io.github.resilience4j.kotlin.retry.executeFunction
+import io.github.resilience4j.retry.Retry
 import no.nav.klage.domain.OidcToken
+import no.nav.klage.util.causeClass
 import no.nav.klage.util.getLogger
+import no.nav.klage.util.rootCause
 import no.nav.slackposter.Severity
 import no.nav.slackposter.SlackClient
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import io.github.resilience4j.retry.Retry
 
 @Component
 class StsClient(
-    private val stsWebClient: WebClient,
-    private val slackClient: SlackClient,
-    private val retrySts: Retry
-    ) {
+        private val stsWebClient: WebClient,
+        private val slackClient: SlackClient,
+        private val retrySts: Retry
+) {
 
     private var cachedOidcToken: OidcToken? = null
 
@@ -32,15 +34,15 @@ class StsClient(
             retrySts.executeFunction {
                 runCatching {
                     cachedOidcToken = stsWebClient.get()
-                        .uri { uriBuilder ->
-                            uriBuilder
-                                .queryParam("grant_type", "client_credentials")
-                                .queryParam("scope", "openid")
-                                .build()
-                        }
-                        .retrieve()
-                        .bodyToMono<OidcToken>()
-                        .block()
+                            .uri { uriBuilder ->
+                                uriBuilder
+                                        .queryParam("grant_type", "client_credentials")
+                                        .queryParam("scope", "openid")
+                                        .build()
+                            }
+                            .retrieve()
+                            .bodyToMono<OidcToken>()
+                            .block()
                 }.onFailure {
                     slackClient.postMessage("Kontakt med sts feilet! (${causeClass(rootCause(it))})", Severity.ERROR)
                 }
@@ -51,8 +53,4 @@ class StsClient(
     }
 
     private fun OidcToken?.shouldBeRenewed(): Boolean = this?.hasExpired() ?: true
-
-    private fun rootCause(t: Throwable): Throwable = t.cause?.run { rootCause(this) } ?: t
-
-    private fun causeClass(t: Throwable) = t.stackTrace[0].className
 }
--- src/main/kotlin/no/nav/klage/clients/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/klage/clients/pdl/PdlClient.kt
@@ -4,21 +4,22 @@ import io.github.resilience4j.kotlin.retry.executeFunction
 import io.github.resilience4j.retry.Retry
 import no.nav.klage.clients.StsClient
 import no.nav.klage.util.TokenUtil
-import no.nav.slackposter.SlackClient
+import no.nav.klage.util.causeClass
+import no.nav.klage.util.rootCause
 import no.nav.slackposter.Severity
+import no.nav.slackposter.SlackClient
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import java.lang.RuntimeException
 
 @Component
 class PdlClient(
-    private val pdlWebClient: WebClient,
-    private val tokenUtil: TokenUtil,
-    private val stsClient: StsClient,
-    private val slackClient: SlackClient,
-    private val retryPdl: Retry
+        private val pdlWebClient: WebClient,
+        private val tokenUtil: TokenUtil,
+        private val stsClient: StsClient,
+        private val slackClient: SlackClient,
+        private val retryPdl: Retry
 ) {
 
     fun getPersonInfo(): HentPdlPersonResponse {
@@ -27,12 +28,12 @@ class PdlClient(
         retryPdl.executeFunction {
             runCatching {
                 results = pdlWebClient.post()
-                    .header(HttpHeaders.AUTHORIZATION, "Bearer ${tokenUtil.getToken()}")
-                    .header("Nav-Consumer-Token", "Bearer ${stsClient.oidcToken()}")
-                    .bodyValue(hentPersonQuery(tokenUtil.getSubject()))
-                    .retrieve()
-                    .bodyToMono<HentPdlPersonResponse>()
-                    .block() ?: throw RuntimeException("Person not found")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer ${tokenUtil.getToken()}")
+                        .header("Nav-Consumer-Token", "Bearer ${stsClient.oidcToken()}")
+                        .bodyValue(hentPersonQuery(tokenUtil.getSubject()))
+                        .retrieve()
+                        .bodyToMono<HentPdlPersonResponse>()
+                        .block() ?: throw RuntimeException("Person not found")
 
             }.onFailure {
                 slackClient.postMessage("Kontakt med pdl feilet! (${causeClass(rootCause(it))})", Severity.ERROR)
@@ -41,8 +42,4 @@ class PdlClient(
 
         return results
     }
-
-    private fun rootCause(t: Throwable): Throwable = t.cause?.run { rootCause(this) } ?: t
-
-    private fun causeClass(t: Throwable) = t.stackTrace[0].className
 }
