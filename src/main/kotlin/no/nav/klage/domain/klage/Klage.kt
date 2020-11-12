4d9c94bfc19aced06c000bddc6a5e04d561cffa8
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2020-11-12 21:36:48 +0100

    Added two fields to Klage model, vedtakDate and vedtakType. Added logic for conversion to and from these and vedtak, ensuring backwards compability.

--- src/main/kotlin/no/nav/klage/domain/klage/Klage.kt
+++ src/main/kotlin/no/nav/klage/domain/klage/Klage.kt
@@ -3,6 +3,7 @@ package no.nav.klage.domain.klage
 import no.nav.klage.domain.Tema
 import no.nav.klage.domain.vedlegg.Vedlegg
 import java.time.Instant
+import java.time.LocalDate
 
 data class Klage(
     val id: Int? = null,
@@ -12,15 +13,22 @@ data class Klage(
     val modifiedByUser: Instant? = Instant.now(),
     val tema: Tema,
     val ytelse: String,
-    val vedtak: String,
+    val vedtak: String? = null,
     val saksnummer: String? = null,
     val vedlegg: List<Vedlegg> = listOf(),
-    val journalpostId: String? = null
+    val journalpostId: String? = null,
+    val vedtakType: VedtakType? = null,
+    val vedtakDate: LocalDate? = null
 )
 
 enum class KlageStatus {
     DRAFT, DONE, DELETED
 }
 
+enum class VedtakType {
+    LATEST, EARLIER
+}
+
 fun Klage.validateAccess(currentIdentifikasjonsnummer: String) = (foedselsnummer == currentIdentifikasjonsnummer)
 fun Klage.isFinalized() = (status === KlageStatus.DONE)
+
--- src/main/kotlin/no/nav/klage/domain/klage/KlageDAO.kt
+++ src/main/kotlin/no/nav/klage/domain/klage/KlageDAO.kt
@@ -3,10 +3,14 @@ package no.nav.klage.domain.klage
 import no.nav.klage.domain.Tema
 import no.nav.klage.domain.vedlegg.VedleggDAO
 import no.nav.klage.domain.vedlegg.Vedleggene
+import no.nav.klage.util.vedtakDateFromVedtak
+import no.nav.klage.util.vedtakFromTypeAndDate
+import no.nav.klage.util.vedtakTypeFromVedtak
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.`java-time`.date
 import org.jetbrains.exposed.sql.`java-time`.timestamp
 import java.time.Instant
 
@@ -23,6 +27,8 @@ class KlageDAO(id: EntityID<Int>) : IntEntity(id) {
     var saksnummer by Klager.saksnummer
     val vedlegg by VedleggDAO referrersOn Vedleggene.klageId
     var journalpostId by Klager.journalpostId
+    var vedtakType by Klager.vedtakType
+    var vedtakDate by Klager.vedtakDate
 }
 
 object Klager : IntIdTable("klage") {
@@ -32,13 +38,15 @@ object Klager : IntIdTable("klage") {
     var modifiedByUser = timestamp("modified_by_user").default(Instant.now())
     var tema = varchar("tema", 3)
     var ytelse = varchar("ytelse", 50)
-    var vedtak = varchar("vedtak", 100)
+    var vedtak = varchar("vedtak", 100).nullable()
     var saksnummer = varchar("saksnummer", 25).nullable()
     var journalpostId = varchar("journalpost_id", 50).nullable()
+    var vedtakType = varchar("vedtak_type", 25).nullable()
+    var vedtakDate = date("vedtak_date").nullable()
 }
 
-fun KlageDAO.toKlage() =
-    Klage(
+fun KlageDAO.toKlage(): Klage {
+    var outputKlage = Klage(
         id = this.id.toString().toInt(),
         foedselsnummer = this.foedselsnummer,
         fritekst = this.fritekst,
@@ -49,17 +57,60 @@ fun KlageDAO.toKlage() =
         vedtak = this.vedtak,
         saksnummer = this.saksnummer,
         vedlegg = this.vedlegg.map { it.toVedlegg() },
-        journalpostId = this.journalpostId
+        journalpostId = this.journalpostId,
+        vedtakType = this.vedtakType.toVedtakType(),
+        vedtakDate = this.vedtakDate
     )
 
-private fun String.toTema() = try {
+    outputKlage = if (this.vedtakType != null || this.vedtakDate != null) {
+        //Fyller ut gammel vedtaksform basert på ny form
+        outputKlage.copy(vedtak = vedtakFromTypeAndDate(this.vedtakType.toVedtakType(), this.vedtakDate))
+    } else if (this.vedtak != null) {
+        //Fyller ut nye vedtaksformer basert på gamle
+        outputKlage.copy(
+            vedtakType = vedtakTypeFromVedtak(this.vedtak!!),
+            vedtakDate = vedtakDateFromVedtak(this.vedtak!!)
+        )
+    } else {
+        outputKlage
+    }
+
+    return outputKlage
+}
+
+fun String.toTema() = try {
     Tema.valueOf(this)
 } catch (e: IllegalArgumentException) {
     Tema.UKJ
 }
 
-private fun String.toStatus() = try {
+fun String.toStatus() = try {
     KlageStatus.valueOf(this)
 } catch (e: IllegalArgumentException) {
     KlageStatus.DRAFT
+}
+
+fun String?.toVedtakType() =
+    if (this != null) VedtakType.valueOf(this) else null
+
+fun KlageDAO.fromKlage(klage: Klage) {
+    var outputVedtakType = klage.vedtakType
+    var outputVedtakDate = klage.vedtakDate
+
+    if (klage.vedtak != null && outputVedtakType == null && outputVedtakDate == null) {
+        outputVedtakDate = vedtakDateFromVedtak(klage.vedtak)
+        outputVedtakType = vedtakTypeFromVedtak(klage.vedtak)
+    }
+
+    foedselsnummer = klage.foedselsnummer
+    fritekst = klage.fritekst
+    status = klage.status.name
+    modifiedByUser = Instant.now()
+    tema = klage.tema.name
+    ytelse = klage.ytelse
+    vedtak = null
+    klage.saksnummer?.let { saksnummer = it }
+    klage.journalpostId?.let { journalpostId = it }
+    vedtakType = outputVedtakType?.name
+    vedtakDate = outputVedtakDate
 }
