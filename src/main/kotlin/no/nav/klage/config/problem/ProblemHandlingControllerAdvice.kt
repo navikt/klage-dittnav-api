47d6230f44d7db65e47b88bb8fd98a65d53352ee
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-01-21 15:17:26 +0100

    Added specific unauthorized exception, and customized logging to avoid warning spam in logs.

--- src/main/kotlin/no/nav/klage/config/problem/ProblemHandlingControllerAdvice.kt
+++ src/main/kotlin/no/nav/klage/config/problem/ProblemHandlingControllerAdvice.kt
@@ -2,6 +2,8 @@ package no.nav.klage.config.problem
 
 import no.nav.klage.domain.exception.*
 import no.nav.klage.util.getLogger
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,8 +11,10 @@ import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.multipart.MaxUploadSizeExceededException
 import org.zalando.problem.Problem
 import org.zalando.problem.Status
+import org.zalando.problem.spring.common.AdviceTraits
 import org.zalando.problem.spring.web.advice.AdviceTrait
 import org.zalando.problem.spring.web.advice.ProblemHandling
+
 
 @ControllerAdvice
 class ProblemHandlingControllerAdvice : OurOwnExceptionAdviceTrait, ProblemHandling
@@ -20,6 +24,17 @@ interface OurOwnExceptionAdviceTrait : AdviceTrait {
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
+    }
+
+    override fun log(
+        throwable: Throwable,
+        problem: Problem?,
+        request: NativeWebRequest?,
+        status: HttpStatus
+    ) {
+        if (throwable is JwtTokenUnauthorizedException) {
+            logger.info("{}: {}", status.reasonPhrase, throwable.message)
+        } else AdviceTraits.log(throwable, status)
     }
 
     @ExceptionHandler
@@ -72,5 +87,11 @@ interface OurOwnExceptionAdviceTrait : AdviceTrait {
         request: NativeWebRequest
     ): ResponseEntity<Problem> =
         create(Status.BAD_REQUEST, ex, request)
-}
 
+    @ExceptionHandler
+    fun handleJwtTokenUnauthorizedException(
+        ex: JwtTokenUnauthorizedException,
+        request: NativeWebRequest
+    ): ResponseEntity<Problem> =
+        create(Status.UNAUTHORIZED, ex, request)
+}
