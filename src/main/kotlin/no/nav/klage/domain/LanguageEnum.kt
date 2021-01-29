ba19811495c77aaff3985476a575d889ae3ed434
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-01-29 13:01:19 +0100

    Removed unused language choice.

--- src/main/kotlin/no/nav/klage/domain/LanguageEnum.kt
+++ src/main/kotlin/no/nav/klage/domain/LanguageEnum.kt
@@ -3,8 +3,6 @@ package no.nav.klage.domain
 import com.fasterxml.jackson.annotation.JsonProperty
 
 enum class LanguageEnum {
-    @JsonProperty("no")
-    NO,
     @JsonProperty("nb")
     NB,
     @JsonProperty("en")
