176e80a461d6897438147e709560cdd3ca103fdd
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-12-17 10:02:37 +0100

    Upgraded Spring Sleuth, added validator.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val oidcSupportVersion = "0.2.18"
 val logstashVersion = "5.1"
 val pdfboxVersion = "2.0.19"
 val tikaVersion = "1.24.1"
-val springSleuthVersion = "2.2.3.RELEASE"
+val springSleuthVersion = "3.0.4"
 val resilience4jVersion = "1.5.0"
 val problemSpringWebStartVersion = "0.26.2"
 val shedlockVersion = "4.23.0"
@@ -45,6 +45,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth:$springSleuthVersion")
 
     implementation("org.projectreactor:reactor-spring:1.0.1.RELEASE")
