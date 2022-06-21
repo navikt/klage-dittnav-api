18ddf3c7ceeb3b2e99df1c0455bec3c284d5fa33
Andreas Jonsson <andreas.jonsson@nav.no>
2022-06-21 14:06:42 +0200

    Debug and config test

--- src/main/kotlin/no/nav/klage/controller/TestController.kt
+++ src/main/kotlin/no/nav/klage/controller/TestController.kt
@@ -1,5 +1,7 @@
 package no.nav.klage.controller
 
+import no.nav.klage.util.getLogger
+import no.nav.klage.util.getSecureLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
+    }
 
     @ProtectedWithClaims(issuer = "tokenx")
     @GetMapping("/test/protected")
@@ -20,9 +28,10 @@ class TestController {
         return "Fritt fram, hei"
     }
 
-    @ProtectedWithClaims(issuer = "tokenx")
+    @ProtectedWithClaims(issuer = "tokenx", claimMap = ["acr=Level4"])
     @GetMapping("/api/test/protected")
     fun test2(): String {
+        logger.debug("Beskyttet fungerte")
         return "Beskyttet, hei"
     }
 
