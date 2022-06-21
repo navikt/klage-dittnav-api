dd9408bebd4f8274d70b9764a36c116c2cec6280
Andreas Jonsson <andreas.jonsson@nav.no>
2022-06-21 13:11:35 +0200

    Added to test api

--- src/main/kotlin/no/nav/klage/controller/TestController.kt
+++ src/main/kotlin/no/nav/klage/controller/TestController.kt
@@ -2,7 +2,8 @@ package no.nav.klage.controller
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.api.Unprotected
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
@@ -16,6 +17,18 @@ class TestController {
     @Unprotected
     @GetMapping("/test/unprotected")
     fun testUbeskyttet(): String {
+        return "Fritt fram, hei"
+    }
+
+    @ProtectedWithClaims(issuer = "tokenx")
+    @GetMapping("/api/test/protected")
+    fun test2(): String {
+        return "Beskyttet, hei"
+    }
+
+    @Unprotected
+    @GetMapping("/api/test/unprotected")
+    fun testUbeskyttet2(): String {
         return "Fritt fram, hei"
     }
 }
