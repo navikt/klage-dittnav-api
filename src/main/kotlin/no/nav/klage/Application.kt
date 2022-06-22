58cf29d6297aafa209a7aa1fcddc240ac31ec4d3
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-06-22 14:13:12 +0200

    Added missing properties.

--- src/main/kotlin/no/nav/klage/Application.kt
+++ src/main/kotlin/no/nav/klage/Application.kt
@@ -1,5 +1,6 @@
 package no.nav.klage
 
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJwtTokenValidation(ignore = ["org.springdoc"])
+@EnableOAuth2Client(cacheEnabled = true)
 @EnableScheduling
 class Application
 
