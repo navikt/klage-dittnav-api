a35b623d566fb400811182e65206386b96f06e24
Andreas Jonsson <andreas.jonsson@nav.no>
2022-07-12 13:19:18 +0200

    Ignore requests to favicon, from correct path.

---
+++ src/main/kotlin/no/nav/klage/controller/FaviconController.kt
@@ -0,0 +1,19 @@
+package no.nav.klage.controller
+
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FaviconController {
+
+    /*
+    Handle request for favicon without token support throwing error.
+    Maybe we shouldn't expose the BE API to the internet at all? Could be that the new rewrite of FE will use
+    a proxy to reach us instead.
+     */
+    @Unprotected
+    @GetMapping("favicon.ico")
+    fun nothingToSeeHere() {
+    }
+}
