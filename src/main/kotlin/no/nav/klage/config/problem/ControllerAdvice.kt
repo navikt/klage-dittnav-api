0b1d6ca67f19b6ac38dac991c6c1f45a5ef24cb1
Andreas Jonsson <andreas.jonsson@nav.no>
2023-01-12 22:56:31 +0100

    Problem logging

--- src/main/kotlin/no/nav/klage/config/problem/ControllerAdvice.kt
+++ src/main/kotlin/no/nav/klage/config/problem/ControllerAdvice.kt
@@ -1,7 +1,7 @@
 package no.nav.klage.config.problem
 
 import no.nav.klage.domain.exception.*
-import no.nav.klage.util.getLogger
+import no.nav.klage.util.getSecureLogger
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
@@ -17,7 +17,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
-        private val logger = getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
     }
 
     @ExceptionHandler
@@ -157,10 +157,28 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
     ): ProblemDetail =
         create(HttpStatus.BAD_REQUEST, ex)
 
-    private fun create(status: HttpStatus, ex: Exception): ProblemDetail {
+    private fun create(httpStatus: HttpStatus, ex: Exception): ProblemDetail {
         val errorMessage = ex.message ?: "No error message available"
-        return ProblemDetail.forStatusAndDetail(status, errorMessage).apply {
+
+        logError(
+            httpStatus = httpStatus,
+            errorMessage = errorMessage,
+            exception = ex
+        )
+
+        return ProblemDetail.forStatusAndDetail(httpStatus, errorMessage).apply {
             title = errorMessage
+        }
+    }
+
+    private fun logError(httpStatus: HttpStatus, errorMessage: String, exception: Exception) {
+        when {
+            httpStatus.is5xxServerError -> {
+                secureLogger.error("Exception thrown to client: ${httpStatus.reasonPhrase}, $errorMessage", exception)
+            }
+            else -> {
+                secureLogger.warn("Exception thrown to client: ${httpStatus.reasonPhrase}, $errorMessage", exception)
+            }
         }
     }
 }
