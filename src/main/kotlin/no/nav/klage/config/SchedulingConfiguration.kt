74cf47f23fa40e5c7e7069c2fe135bcd44079bc1
Andreas Jonsson <andreas.jonsson@nav.no>
2024-02-15 13:04:22 +0100

    WIP JPA rewrite. Test cron.

--- src/main/kotlin/no/nav/klage/config/SchedulingConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/SchedulingConfiguration.kt
@@ -7,12 +7,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 import javax.sql.DataSource
 
 
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "10m")
 @EnableAsync
+@EnableScheduling
 class SchedulingConfiguration {
 
     @Bean
