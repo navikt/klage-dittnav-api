69da0a7356673fa67489207fc3f4294a5d680642
Andreas Jonsson <andreas.jonsson@nav.no>
2024-02-26 15:38:47 +0100

    Correct filename for generated pdf.

--- src/main/kotlin/no/nav/klage/controller/PdfController.kt
+++ src/main/kotlin/no/nav/klage/controller/PdfController.kt
@@ -1,7 +1,8 @@
 package no.nav.klage.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
-import no.nav.klage.controller.view.*
+import no.nav.klage.controller.view.OpenEttersendelseInput
+import no.nav.klage.controller.view.OpenKlankeInput
 import no.nav.klage.domain.Type
 import no.nav.klage.service.KlageDittnavPdfgenService
 import no.nav.klage.util.getLogger
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/pdf")
 class PdfController(
     private val klageDittnavPdfgenService: KlageDittnavPdfgenService
-){
+) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -88,7 +89,7 @@ class PdfController(
 
         val responseHeaders = HttpHeaders()
         responseHeaders.contentType = MediaType.valueOf("application/pdf")
-        responseHeaders.add("Content-Disposition", "inline; filename=anke.pdf")
+        responseHeaders.add("Content-Disposition", "inline; filename=${input.type!!.name.lowercase()}.pdf")
         return ResponseEntity(
             content,
             responseHeaders,
