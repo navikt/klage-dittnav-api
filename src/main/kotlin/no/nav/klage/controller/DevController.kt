49b825f3ac68c8c1c0e02428b674aa7f4d9434ad
Andreas Jonsson <andreas.jonsson@nav.no>
2026-01-04 16:54:01 +0100

    Use new shared workflows.

--- src/main/kotlin/no/nav/klage/controller/DevController.kt
+++ src/main/kotlin/no/nav/klage/controller/DevController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
-@Profile("dev-gcp")
+@Profile("dev")
 @RestController
 class DevController(private val klageDittnavPdfgenService: KlageDittnavPdfgenService) {
 
