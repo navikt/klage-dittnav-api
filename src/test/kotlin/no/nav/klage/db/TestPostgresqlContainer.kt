1ae9a2ee950a8a9b33d4654eddddb47e8ce0a020
Andreas Jonsson <andreas.jonsson@nav.no>
2026-01-29 12:25:54 +0100

    Upgrade to Postgres 17 in dev.

--- src/test/kotlin/no/nav/klage/db/TestPostgresqlContainer.kt
+++ src/test/kotlin/no/nav/klage/db/TestPostgresqlContainer.kt
@@ -7,7 +7,7 @@ import org.testcontainers.postgresql.PostgreSQLContainer
 
 abstract class PostgresIntegrationTestBase {
     companion object {
-        private val postgres = PostgreSQLContainer("postgres:16.0").apply {
+        private val postgres = PostgreSQLContainer("postgres:17.0").apply {
             waitingFor(HostPortWaitStrategy())
             withReuse(true)
             start()
