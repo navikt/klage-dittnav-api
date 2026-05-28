2cb46f0e6f0c1ab493d52c9357ba748edc527781
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-05-28 08:35:04 +0200

    Fix vulnerabilities, while waiting for fix in Spring Boot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webclient")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
 
+    //Fix vulnerabilities, while waiting for fix in Spring Boot.
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.22")
+    implementation("io.netty:netty-codec-http:4.2.14.Final")
+    implementation("io.netty:netty-codec-http2:4.2.14.Final")
+    implementation("io.netty:netty-codec-http3:4.2.14.Final")
+    implementation("io.netty:netty-codec-dns:4.2.14.Final")
+    implementation("io.netty:netty-codec-compression:4.2.14.Final")
+    implementation("io.netty:netty-transport-native-epoll:4.2.14.Final")
+    implementation("io.netty:netty-resolver-dns:4.2.14.Final")
+
     implementation("io.micrometer:micrometer-registry-prometheus")
 
     implementation("io.opentelemetry:opentelemetry-api:$otelVersion")
