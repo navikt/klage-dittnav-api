358e9bc19b1be9e58e822f045a1df7c28a7175fd
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-08-20 09:22:23 +0200

    Adjust response class.

--- src/main/kotlin/no/nav/klage/clients/safselvbetjening/GetDokumentoversiktResponse.kt
+++ src/main/kotlin/no/nav/klage/clients/safselvbetjening/GetDokumentoversiktResponse.kt
@@ -1,8 +1,8 @@
 package no.nav.klage.clients.safselvbetjening
 
-data class GetDokumentoversiktResponse(val data: Dokumentoversikt?, val errors: List<PdlError>?)
-
-data class Dokumentoversikt(val tema: List<TemaFromSafselvbetjening>?)
+data class GetDokumentoversiktResponse(val data: DokumentoversiktSelvbetjeningResponse?, val errors: List<PdlError>?)
+data class DokumentoversiktSelvbetjeningResponse( val dokumentoversiktSelvbetjening: DokumentoversiktSelvbetjening)
+data class DokumentoversiktSelvbetjening(val tema: List<TemaFromSafselvbetjening>)
 
 data class TemaFromSafselvbetjening(
     val kode: String?,
