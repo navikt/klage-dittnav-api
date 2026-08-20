d33312fcaff2209d79151065af05ebe72e61cd5b
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-08-20 08:44:41 +0200

    Different tema handling.

--- src/main/kotlin/no/nav/klage/clients/safselvbetjening/GetDokumentoversiktRequest.kt
+++ src/main/kotlin/no/nav/klage/clients/safselvbetjening/GetDokumentoversiktRequest.kt
@@ -9,7 +9,7 @@ data class GetDokumentoversiktGraphqlQuery(
 
 data class GetDokumentoversiktVariables(
     val ident: String,
-    val tema: String = "SYK"
+    val tema: List<Tema> = listOf(Tema.SYK),
 )
 
 fun getDokumentoversiktQuery(ident: String): GetDokumentoversiktGraphqlQuery {
@@ -19,4 +19,75 @@ fun getDokumentoversiktQuery(ident: String): GetDokumentoversiktGraphqlQuery {
         query = query,
         variables = GetDokumentoversiktVariables(ident = ident)
     )
+}
+
+enum class Tema {
+    AAP, //Arbeidsavklaringspenger
+    AAR, //Aa-registeret
+    AGR, //Ajourhold - Grunnopplysninger
+    AKT, //Aktivitetsplan med dialoger
+    ARP, //Arbeidsrådgivning - psykologtjenester
+    ARS, //Arbeidsrådgivning - skjermet
+    BAR, //Barnetrygd
+    BBF, //Barnebortføring
+    BID, //Bidrag
+    BIL, //Bil
+    DAG, //Dagpenger
+    ENF, //Enslig forsørger
+    ERS, //Erstatning
+    EYB, //Barnepensjon
+    EYO, //Omstillingsstønad
+    FAR, //Farskap
+    FEI, //Feilutbetaling
+    FIP, //Fiskerpensjon
+    FOR, //Foreldre- og svangerskapspenger
+    FOS, //Forsikring
+    FRI, //Kompensasjon for selvstendig næringsdrivende/frilansere
+    FUL, //Fullmakt
+    GEN, //Generell
+    GRA, //Gravferdsstønad
+    GRU, //Grunn- og hjelpestønad
+    HEL, //Helsetjenester og ortopediske hjelpemidler
+    HJE, //Hjelpemidler
+    IAR, //Inkluderende arbeidsliv
+    IND, //Tiltakspenger
+    KLL, //Klage - lønnsgaranti
+    KON, //Kontantstøtte
+    KTA, //Kontroll - anmeldelse
+    KTR, //Kontroll
+    MED, //Medlemskap
+    MOB, //Mobilitetsfremmende stønad
+    OLJ, //Oljepionerene
+    OMS, //Omsorgspenger, pleiepenger og opplæringspenger
+    OPA, //Oppfølging - Arbeidsgiver
+    OPP, //Oppfølging
+    PAI, //Innsyn
+    PEN, //Pensjon
+    PER, //Permittering og masseoppsigelser
+    POI, //Innsyn etter personopplysningsloven
+    REH, //Rehabilitering
+    REK, //Rekruttering og stilling
+    RPO, //Retting av personopplysninger
+    RVE, //Rettferdsvederlag
+    SAA, //Sanksjon - Arbeidsgiver
+    SAK, //Saksomkostninger
+    SAP, //Sanksjon - Person
+    SER, //Serviceklager
+    SIK, //Sikkerhetstiltak
+    STO, //Regnskap/utbetaling
+    SUP, //	Supplerende stønad
+    SYK, //Sykepenger
+    SYM, //Sykmeldinger
+    TIL, //Tiltak
+    TRK, //Trekkhåndtering
+    TRY, //Trygdeavgift
+    TSO, //Tilleggsstønad
+    TSR, //Tilleggsstønad arbeidssøkere
+    UFM, //Unntak fra medlemskap
+    UFO, //Uføretrygd
+    UKJ, //Ukjent
+    UNG, //Ungdomsprogramytelsen
+    VEN, //Ventelønn
+    YRA, //Yrkesrettet attføring
+    YRK //Yrkesskade / Menerstatning
 }
